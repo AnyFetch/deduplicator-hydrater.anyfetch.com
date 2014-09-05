986a88109b83a1f75e7bf9e5fb77239b3910b062
buathi_q <buathi_q@epitech.eu>
2014-09-05 11:51:11 +0200

    Add a test to check that the hydrater start without errors

---
+++ test/test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('should');
+
+describe('Test starting', function() {
+  it('should start without error', function(done) {
+    var config = require("../config/configuration.js");
+    var server = require('../app.js');
+
+    // Start the server
+    server.listen(config.port, function() {
+      console.log("Server listening on " + server.url);
+      done();
+    });
+  });
+});
